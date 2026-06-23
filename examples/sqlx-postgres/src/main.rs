aa09e8f8edf335005b71bf702c5aa257e07b1c96
vicotrbb <victor.bona@hotmail.com>
2026-06-23 19:35:00 -0300

    feat: use sqlx facade in postgres example

--- examples/sqlx-postgres/src/main.rs
+++ examples/sqlx-postgres/src/main.rs
@@ -1,5 +1,4 @@
-use nidus_core::{Container, Inject};
-use sqlx::postgres::PgPoolOptions;
+use nidus::prelude::{Container, Inject, PgPoolOptions, ProviderLifetime};
 
 struct DatabaseOptions(PgPoolOptions);
 
@@ -8,18 +7,37 @@ struct UsersRepository {
     options: Inject<DatabaseOptions>,
 }
 
-fn main() {
+fn container() -> Container {
     let mut container = Container::new();
     container
         .register_singleton(DatabaseOptions(PgPoolOptions::new()))
         .unwrap();
     container
-        .register_factory(nidus_core::ProviderLifetime::Singleton, |container| {
+        .register_factory(ProviderLifetime::Singleton, |container| {
             Ok(UsersRepository {
                 options: container.inject::<DatabaseOptions>()?,
             })
         })
         .unwrap();
+    container
+}
+
+fn main() {
+    let container = container();
     let repository = container.resolve::<UsersRepository>().unwrap();
     let _options = &repository.options.0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn resolves_repository_with_sqlx_options() {
+        let container = container();
+
+        let repository = container.resolve::<UsersRepository>().unwrap();
+
+        let _options = &repository.options.0;
+    }
 }
